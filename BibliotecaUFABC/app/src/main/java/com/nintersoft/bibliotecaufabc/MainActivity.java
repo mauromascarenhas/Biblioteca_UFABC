1cd8b23d18b4b3ca74bef85a533195bbcc1ce1af
Mauro Mascarenhas <mauro.mascarenhas@nintersoft.com>
2019-09-02 23:01:01 -0300

    Removed unnused import

--- BibliotecaUFABC/app/src/main/java/com/nintersoft/bibliotecaufabc/MainActivity.java
+++ BibliotecaUFABC/app/src/main/java/com/nintersoft/bibliotecaufabc/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
